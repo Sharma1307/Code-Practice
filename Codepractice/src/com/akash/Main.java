4d6ef6d9ce279e8d5e720026671a9f246c44a716
akash <akashsh1307@gmial.com>
2024-09-01 18:16:12 +0530

    First project for the Encapsulation.

---
+++ Codepractice/src/com/akash/Main.java
@@ -0,0 +1,17 @@
+package com.akash;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Person P1 = new Person("Abhishek", 23, "Male");
+
+		P1.setName("Akash");
+
+		System.out.println("Name " + P1.getName());
+
+		System.out.println(P1);
+
+	}
+
+}
